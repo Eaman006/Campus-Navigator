bd1c0c6cb54459619c15266066cf82c5650b527b
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-03-12 23:18:40 +0530

    login annimation added

--- my-app/app/login/page.js
+++ my-app/app/login/page.js
@@ -3,11 +3,20 @@ import Image from 'next/image'
 
 const page = () => {
   return (
-    <div>
-        <div className='absolute bottom-0'>
-            <Image width={1920} height={1080} alt='background' src="/login.png" />
+    <div className='relative w-full h-screen'>
+        <div className='w-full h-full'>
+            <video className='w-full h-[40%] object-cover' loop autoPlay muted src="/login.mp4" />
         </div>
-      
+        <div className='absolute bottom-0 w-screen h-[80vh]'>
+    <Image 
+        width={1920} 
+        height={800} 
+        alt='background' 
+        src="/login.png" 
+        className="w-full h-full object-cover" 
+    />
+</div>
+
     </div>
   )
 }
