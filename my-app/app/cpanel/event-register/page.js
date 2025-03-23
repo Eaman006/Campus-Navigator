74f603bad095e6c6a82cf0c25ebfc24a67bca120
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-03-23 19:50:37 +0530

    event registration under construction

--- my-app/app/cpanel/event-register/page.js
+++ my-app/app/cpanel/event-register/page.js
@@ -1,11 +1,60 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const page = () => {
+  const [venueBuilding, setVenueBuilding] = useState("");
+
+  const handleSelectChange = (e) => {
+    setVenueBuilding(e.target.value);
+  };
+
   return (
-    <div>
-      
+    <div className='text-black pl-20'>
+      <div className='font-bold text-center text-xl m-2'>
+        Event Registration
+      </div>
+      <div className='mx-10 shadow-md shadow-black'>
+        <form className='m-3 p-3' action="">
+          <div className='text-center mb-4 font-bold text-xl'>Event Details</div>
+
+          <div className='mx-2'>Event Name</div>
+          <input type="text" placeholder='Enter the event name' className="ml-2 mb-4 p-2 border border-gray-300 rounded-lg" />
+
+          <div className='mx-2'>Event Date</div>
+          <input type="date" className="ml-2 mb-4 p-2 border border-gray-300 rounded-lg" />
+
+          <div className='mx-2'>Event Start Time</div>
+          <input type="time" className="ml-2 mb-4 p-2 border border-gray-300 rounded-lg" />
+
+          <div className='mx-2'>Event End Time</div>
+          <input type="time" className="ml-2 mb-4 p-2 border border-gray-300 rounded-lg" />
+
+          <div className='mx-2'>Venue Building</div>
+          <select
+            value={venueBuilding}
+            onChange={handleSelectChange}
+            className="ml-2 mb-4 p-2 border border-gray-300 rounded-lg"
+          >
+            <option value="" disabled>Select a building</option>
+            <option value="Building A">Academic Block 1</option>
+            <option value="Building B">Academic Block 2</option>
+            <option value="Building C">Architecture Building</option>
+            <option value="Building D">Lab Complex</option>
+          </select>
+          <div className='mx-2'>Venue Room</div>
+          <input type="text" placeholder='Enter room number' className="ml-2 mb-4 p-2 border border-gray-300 rounded-lg" />
+          <div className='mx-2'>Coordinator Name</div>
+          <input type="text" placeholder='Enter Teacher Coordinator Name' className="ml-2 mb-4 p-2 border border-gray-300 rounded-lg" />
+          <div className='flex gap-5'>
+            <button>Reset</button>
+            <button>Submit</button>
+
+          </div>
+        </form>
+      </div>
+
     </div>
   )
 }
 
-export default page
+export default page;
--- my-app/app/cpanel/room/page.js
+++ my-app/app/cpanel/room/page.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className=' text-black pl-20'>
+      <div className='font-bold text-center text-xl m-2'>
+        Check Room Availability
+      </div>
       
     </div>
   )
