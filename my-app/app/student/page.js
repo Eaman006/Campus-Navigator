5bfb000d9a7cad3cc044f92108a4a49b2382b069
Navnit <178172653+Navnit-07@users.noreply.github.com>
2025-03-23 22:01:43 +0530

    add search professor by name functionality

--- my-app/app/student/page.js
+++ my-app/app/student/page.js
@@ -32,6 +32,9 @@ function Page() {
   const [start, setStart] = useState("");
   const [end, setEnd] = useState("");
   const [svgData, setSvgData] = useState(null);
+  const [teacherName, setTeacherName] = useState('');
+  const [teacherDetails, setTeacherDetails] = useState({});
+  const [isTeacherDetailPopupOpen, setIsTeacherDetailPopupOpen] = useState(false);
   const mapRef = useRef(null);
   const inputRef = useRef(null);
   const router = useRouter();
@@ -260,6 +263,36 @@ function Page() {
         <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
       </div>
     );
+  }
+
+  // Logic to find teacher's detail by their name
+
+  function teacherChangeHandler(e){
+    setTeacherName(e.target.value)
+  }
+
+  async function searchTeacher(){
+    const response = await fetch(`https://project-expo-group-90-production.up.railway.app/search_teacher?teacher_name=${teacherName}`)
+
+    if(response.ok){
+
+      const data = await response.json();
+
+      if (data.error) {
+        alert(data.error);
+        return;
+      }
+
+      // save value for pop-up to show teacher details
+      setTeacherDetails(data)
+      setIsTeacherDetailPopupOpen(true)
+      console.log(data.Cabin)
+      setTeacherName('')
+    }
+  }
+
+  function teacherSearchHandler(e){
+    e.key === "Enter" && searchTeacher()
   }
 
   return (
@@ -559,7 +592,14 @@ function Page() {
         <div className={style['container-right']}>
             <div className={style['sub-container-right']}>
             <div className={style['input-container']} onClick={handleFocus}>
-              <input ref={inputRef} type="text" placeholder='Search any floor' />
+              <input 
+                ref={inputRef} 
+                type="text" 
+                placeholder='Search any professor' 
+                value = {teacherName} 
+                onChange={teacherChangeHandler}
+                onKeyDown={teacherSearchHandler}
+              />
                     <div className={style['input-logo-container']}>
                         <Image src={Search} width={50} height={50} alt='logo' />
                 <Image onClick={handleSearchOpen} className='cursor-pointer hover:scale-110 duration-300' src={Direction} width={50} height={50} alt='logo' />
@@ -597,6 +637,25 @@ function Page() {
               <Link href="/chatbot"><Image src='/Chatbot.png' width={45} height={45} alt='logo' /></Link>
             </div>
           </div>
+
+          {/* Popup Modal */}
+      {isTeacherDetailPopupOpen && teacherDetails && (
+        <div className="absolute top-20 left-30 flex items-center justify-center bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+            <h2 className="text-xl font-bold mb-4">Professor Details</h2>
+            <p><strong>Cabin:</strong> {teacherDetails.Cabin || "N/A"}</p>
+            <p><strong>Matched Name:</strong> {teacherDetails.Matched_Name || "N/A"}</p>
+            <p><strong>Phone Number:</strong> {teacherDetails["Phone Number"] || "N/A"}</p>
+            <p><strong>Room No:</strong> {teacherDetails.Room_No || "N/A"}</p>
+
+            <button
+              onClick={() => setIsTeacherDetailPopupOpen(false)}
+              className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 cursor-pointer"
+            >
+              Close
+            </button>
+          </div>
+        </div>)}
 
           {/* Floor Map Display */}
           <div className="w-full h-[calc(100%-4rem)] mt-4 overflow-hidden">
