3585116952d73e628be5c746f7952fba6bac28a5
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-04-15 18:54:15 +0530

    Details for lab comp floor 1 added

---
+++ my-app/app/data/lab/Floor1.js
@@ -0,0 +1,9 @@
+const floor1Data = {
+    rect6: { details: "Room 101: PLACEMENT LAB<br>Features: Air Conditioned, Projector, Wi-Fi", hasTeachers: false },
+    rect10: { details: "Room 102: PLACEMENT LAB<br>Features: Air Conditioned, Projector, Wi-Fi", hasTeachers: false },
+    rect11: { details: "Room 103: Classroom<br>Features: Air Conditioned, Projector, Wi-Fi", hasTeachers: false },
+    rect12: { details: "Room 104: CYBER SECURITYAND DIGITAL FORENCICS CENTER<br>Features: Desks, Computers, Wi-Fi", hasTeachers: false },
+    rect13: { details: "Room 105: AI IN HEALTH CARE LABORATORY", hasTeachers: false },
+  };
+  
+  export default floor1Data; 
